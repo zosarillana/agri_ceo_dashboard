eaaaa0b9f947999bbda03c1340e37386f4e9991d
zosarillana <zhou991818@gmail.com>
2026-04-20 15:19:08 +0800

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(), // Add this plugin
+  ],
 })
